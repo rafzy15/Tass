d37c4ad50171cfb896e839d4a6f162f59428eb74
Rafal <rafzy15@gmail.com>
2016-12-21 11:16:54 +0100

    poprawki

--- src/main/java/com/wut/JsonParser.java
+++ src/main/java/com/wut/JsonParser.java
@@ -5,65 +5,77 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
 /**
  * Created by rafal on 12/14/16.
  */
 public class JsonParser {
-    String json = "{\"total\":3,\"_links\":{\"self\":{\"href\":\"https://api.goldenline.pl/users/43549/contacts\"},\"owner\":{\"href\":\"https://api.goldenline.pl/users/43549\",\"title\":\"Contacts owner\"}},\"_embedded\":{\"contacts\":[{\"id\":100,\"name\":\"Pomoc\",\"surname\":\"GoldenLine\",\"photo\":\"https://static.goldenline.pl/user_photo/100/user_100_d5c5f5_huge.jpg\",\"_links\":{\"self\":{\"href\":\"https://api.goldenline.pl/users/100\"},\"www\":{\"href\":\"https://www.goldenline.pl/goldenline-pomoc/\",\"title\":\"User profile WWW address\",\"type\":\"text/html\"}}},{\"id\":23558,\"name\":\"Grażyna\",\"surname\":\"Szydłowska Ex Janiak\",\"photo\":\"https://static.goldenline.pl/user_photo/006/user_23558_18fc2b_huge.jpg\",\"_links\":{\"self\":{\"href\":\"https://api.goldenline.pl/users/23558\"},\"www\":{\"href\":\"https://www.goldenline.pl/grazyna-janiak/\",\"title\":\"User profile WWW address\",\"type\":\"text/html\"}}},{\"id\":39874,\"name\":\"Mar\",\"surname\":\"Galt\",\"photo\":\"https://static.goldenline.pl/user_man_photo_huge.png\",\"_links\":{\"self\":{\"href\":\"https://api.goldenline.pl/users/39874\"},\"www\":{\"href\":\"https://www.goldenline.pl/39874/\",\"title\":\"User profile WWW address\",\"type\":\"text/html\"}}}]}}";
-    private List<String> toVisitNeighbours = new LinkedList<String>();
-    private Stack<String> visited = new Stack<String>();
+    private Stack<String> toDo = new Stack<String>();
+    private Set<String> done = new HashSet<String>();
 
+    private int nrAddedRows = 0;
+    private boolean lastPage;
 
     public void visitFriendsOnePage(Elements friendContactList, String userUrl) {
 //        Elements friendContactList = getUrlContactsFromOnePage(userUrl + pageContactNumber);
 
         for (Element friendRow : friendContactList) {
             String hrefToFriend = friendRow.attr("href");
-//            for (String linkvisited : visited) {
-//                if (!linkvisited.contains(hrefToFriend)) {
-                    toVisitNeighbours.add(hrefToFriend);
-                    visited.add(hrefToFriend);
-//                }
-                System.out.println("friend " + hrefToFriend);
+            done.add(userUrl);
+            for (String linkvisited : done) {
+                if (!linkvisited.contains(hrefToFriend)) {
+                    toDo.add(hrefToFriend);
+                }
 
-                saveToFile(userUrl + " , " + hrefToFriend);
-//            }
+            }
 
+            saveToFile(userUrl + " , " + hrefToFriend);
+            nrAddedRows++;
+
+        }
+        if (nrAddedRows > 800 && lastPage) {
+            try {
+                Thread.sleep(3* 60 * 1000);
+                System.out.println("I'm sleeping for 3 minuts");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public void visitFriendsInAllPages(String userUrl) {
         try {
-            Document doc = Jsoup.connect(userUrl+"kontakty/s/1").get();
+            Document doc = Jsoup.connect(userUrl + "kontakty/s/1").get();
             Elements pagerElement = doc.select("ul:not(#contactLetters).pager").select("li");
             int numberOfPages = 0;
             if (pagerElement.size() > 1) {
                 Element lastPage = pagerElement.get(pagerElement.size() - 2);
-                numberOfPages =  Integer.parseInt(lastPage.select("a").html());
+                numberOfPages = Integer.parseInt(lastPage.select("a").html());
             }
             Elements friendHrefList = getUrlContactsFromOnePage(doc);
             visitFriendsOnePage(friendHrefList, userUrl);
-            for(int i = 0; i < numberOfPages;i++) {
+            lastPage = false;
+
+            for (int i = 2; i <= numberOfPages; i++) {
                 try {
-                    Thread.sleep(10*1000);
+                    Thread.sleep(5 * 1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                doc = Jsoup.connect(userUrl + "kontakty/s/"+i).get();
+                doc = Jsoup.connect(userUrl + "kontakty/s/" + i).get();
                 friendHrefList = getUrlContactsFromOnePage(doc);
                 visitFriendsOnePage(friendHrefList, userUrl);
-
+                if (i == numberOfPages) {
+                    lastPage = true;
+                }
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+//        }
 
     }
 
@@ -80,16 +92,60 @@ public class JsonParser {
 
     //przeszukiwanie grafu wszerz
     public void searchGraph(String startUrl) {
-//        toVisitNeighbours.add(startUrl);
-        visited.add(startUrl);
-        visitFriendsInAllPages(startUrl);
-        while (!toVisitNeighbours.isEmpty()) {
-            String nextVisit = toVisitNeighbours.remove(0);
+//        toDo.add(startUrl);
+//        done.add(startUrl);
+//        visitFriendsInAllPages(startUrl);
+        while (!toDo.isEmpty()) {
+            String nextVisit = toDo.remove(0);
             System.out.println(nextVisit);
             visitFriendsInAllPages(nextVisit);
+
         }
 
     }
+
+    public void loadDone() {
+        BufferedReader br = null;
+        FileReader fr = null;
+
+        try {
+            br = new BufferedReader(new FileReader("out.txt"));
+            String currentLine;
+            while ((currentLine = br.readLine()) != null) {
+                String visited = currentLine.split(",")[0].trim();
+                String toVisit = currentLine.split(",")[1].trim();
+                this.done.add(visited);
+                boolean addToDo = true;
+                this.toDo.add(toVisit);
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null)
+                    br.close();
+                if (fr != null)
+                    fr.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+        }
+        // remove the same elements in To doList
+        Set<String> hs = new LinkedHashSet<String>();
+        hs.addAll(toDo);
+        toDo.clear();
+        toDo.addAll(hs);
+        // remove element which is done
+        toDo.removeAll(done);
+        System.out.println(done);
+        System.out.println(toDo);
+
+
+    }
+
 
     private void saveToFile(String line) {
         System.out.println(line);
--- src/main/java/com/wut/Main.java
+++ src/main/java/com/wut/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         JsonParser jsonParser = new JsonParser();
+        jsonParser.loadDone();
         jsonParser.searchGraph("http://www.goldenline.pl/anna-kowalska172/");
+//        jsonParser.add();
+
     }
 }
