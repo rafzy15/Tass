b384184886c690c5769a50f5a20204aeb8b4133c
rafzy15 <rafzy15@gmail.com>
2019-02-24 19:09:55 +0100

    Update Main.java
    Remive some comments

--- src/main/java/com/wut/Main.java
+++ src/main/java/com/wut/Main.java
@@ -5,9 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         FriendsParser friendsParser = new FriendsParser();
-//        friendsParser.loadDone();
-        //last done http://www.goldenline.pl/monika-andrzejuk2/kontakty/s/88
-//        friendsParser.searchGraph("http://www.goldenline.pl/katarzyna-cioczek/");
         InfoParser infoParser = new InfoParser();
         infoParser.connectToDatabase();
 //
